3a6d4cb24419d65cada92543828c496d413e592f
Najim Rizky <najim.rizky@student.umn.ac.id>
2021-07-18 15:32:32 +0700

    Upgrade dikit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 // import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home'
+import 'semantic-ui-css/semantic.min.css'
+// import pkg from 'semantic-ui-react/package.json'
 
 function App() {
   
   return (
     <div className="App">
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
       <Home/>
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 // import React from 'react'
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
-
+import { Grid, Container, Header } from 'semantic-ui-react'
 
 function Home() {
     const [latitude, setLatitude] = useState(null)
@@ -10,13 +10,13 @@ function Home() {
     const [weather, setWeather] = useState(null)
 
     useEffect(() => {
-        if(!latitude && !longitude) getLatLon() 
-        if(!!latitude && !!longitude && !location) getLocation() 
-        if(!!latitude && !!longitude && !!location && !weather) getWeather()
+        if (!latitude && !longitude) getLatLon()
+        if (!!latitude && !!longitude && !location) getLocation()
+        if (!!latitude && !!longitude && !!location && !weather) getWeather()
         // eslint-disable-next-line
     }, [latitude, longitude, location, weather])
 
-    function getLatLon(){
+    function getLatLon() {
         navigator.geolocation.getCurrentPosition(function (position) {
             setLatitude(position.coords.latitude)
             setLongitude(position.coords.longitude)
@@ -24,14 +24,11 @@ function Home() {
     }
 
     function getLocation() {
-        axios.get('https://us1.locationiq.com/v1/reverse.php?key=pk.43eca1a5c71fa8c277ecef1c575eb353&lat='+latitude+'&lon='+longitude+'&format=json')
+        axios.get('https://us1.locationiq.com/v1/reverse.php?key=pk.43eca1a5c71fa8c277ecef1c575eb353&lat=' + latitude + '&lon=' + longitude + '&format=json')
             .then(function (response) {
                 // handle success
                 let data = response.data
-                if(data['address']['state_district'] === undefined)
-                    setLocation(data['address']['county'] + ", " + data['address']['city'] + ", " + data['address']['state'])
-                else
-                    setLocation(data['address']['county'] + ", " + data['address']['city'] + ", " + data['address']['state_district'])
+                setLocation(data)
             })
             .catch(function (error) {
                 // handle error
@@ -42,7 +39,7 @@ function Home() {
             });
     }
 
-    function getWeather(){
+    function getWeather() {
         axios.get('https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&exclude=minutely,daily&units=metric&lang=id&appid=98cbf771881782a7fd24dc4d2219599a')
             .then(function (response) {
                 // handle success
@@ -60,12 +57,33 @@ function Home() {
     return (
         <>{
             weather ? (
-                <div>
-                    <h1>{location}</h1>
-                    <h4>{weather['current']['weather'][0]['description']}</h4>
-                </div>
+                <Container>
+                    <Grid divided='vertically' padded>
+                        <Grid.Row columns={2}>
+                            <Grid.Column textAlign='left' width={4} >
+                                <h4>{weather['current']['weather'][0]['description']}</h4>
+                            </Grid.Column>
+                            <Grid.Column textAlign='right' width={12}>
+                                <p style={{fontSize: '22px'}}>{location['address']['county'] + ", " + location['address']['city']}
+                                <br/>
+                                {   
+                                    location['address']['state_district'] === undefined ? (
+                                        location['address']['state']
+                                    ) : (
+                                        location['address']['state_district']
+                                    )
+                                }
+                                <br/>
+                                <span style={{fontSize: '18px', color: 'gray'}}>{location['address']['country']}</span>
+                                </p>
+                            </Grid.Column>
+                        </Grid.Row>
+                    </Grid>
+                </Container>
             ) : (
-                <></>
+                <>
+                    Loading
+                </>
             )
         }</>
     )
