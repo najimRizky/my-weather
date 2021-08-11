8895a1e7a7db2f7e604904881f7a0a40e17abaf4
Najim Rizky <najim.rizky@student.umn.ac.id>
2021-08-11 14:59:01 +0700

    Add Router

--- src/App.js
+++ src/App.js
@@ -4,14 +4,23 @@ import Home from './pages/Home'
 import Header from './components/Header'
 import 'semantic-ui-css/semantic.min.css'
 // import pkg from 'semantic-ui-react/package.json'
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom'
+import MyLocation from './components/MyLocation';
 
 function App() {
   
   return (
     <div className="App">
       {/* <br/><br/><br/><br/> */}
-      <Header/>
-      <Home/>
+      <Router>
+        <Header/>
+        <Switch>
+          <Route path="/" exact component={MyLocation}></Route>
+          <Route path="/search" exact component={Home}></Route>
+          <Redirect path="*" to="/"/>
+        </Switch>
+      </Router>
       {/* <br/><br/><br/><br/> */}
     </div>
   );
