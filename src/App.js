bfc49f0c2bb744b0c615716dcb237683c46798c5
Najim Rizky <najim.rizky@student.umn.ac.id>
2021-08-11 20:40:20 +0700

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import 'semantic-ui-css/semantic.min.css'
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom'
 import MyLocation from './components/MyLocation';
+import Nav from './components/Nav'
 
 function App() {
   
@@ -15,6 +16,7 @@ function App() {
       {/* <br/><br/><br/><br/> */}
       <Router>
         <Header/>
+        <Nav/>
         <Switch>
           <Route path="/" exact component={MyLocation}></Route>
           <Route path="/search" exact component={Home}></Route>
---
+++ src/components/Nav.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import { Container, Grid, Menu, Segment } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
+import './../App.css'
+
+
+
+export default class MenuExampleInvertedSecondary extends Component {
+  state = window.location.pathname === "/" ? {activeItem: "Your Location"} : {activeItem: "Search Other Location"}
+  handleItemClick = (e, { name }) => {
+    this.setState({ activeItem: name })
+    
+  }
+  render() {
+    const { activeItem } = this.state 
+    // const location = useLocation()
+
+    return (
+      <Container>
+        <Grid centered textAlign="center">
+          <Grid.Column mobile={12} computer={8}>
+            <Segment style={{border: "none", boxShadow: "none"}}>
+              <Menu  pointing secondary widths={2}>
+                <Menu.Item
+                  name='Your Location'
+                  active={activeItem === 'Your Location'}
+                  onClick={this.handleItemClick}
+                  as={Link}
+                  to="/"
+                />
+                <Menu.Item
+                  name='Search Other Location'
+                  active={activeItem === 'Search Other Location'}
+                  onClick={this.handleItemClick}
+                  as={Link}
+                  to="/search"
+                />
+              </Menu>
+            </Segment>
+          </Grid.Column>
+        </Grid>
+      </Container>
+    )
+  }
+}
